f01277b64edccc140c44f2e2db0e41fecb3479e6
ryostar <82922436+ryostar@users.noreply.github.com>
2021-07-01 04:55:22 +0700

    Update fakeit.py

--- LaylaRobot/modules/fakeit.py
+++ LaylaRobot/modules/fakeit.py
@@ -14,10 +14,10 @@ async def hi(event):
         return
     if event.is_group:
         if not await is_admin(event, event.message.sender_id):
-            await event.reply("`You Should Be Admin To Do This!`")
+            await event.reply("`Bạn Nên Làm Quản Trị Viên Để Làm Điều Này!`")
             return
     fake = Faker()
-    print("FAKE DETAILS GENERATED\n")
+    print("CÁC CHI TIẾT GIẢ ĐÃ ĐƯỢC TẠO\n")
     name = str(fake.name())
     fake.add_provider(internet)
     address = str(fake.address())
@@ -28,7 +28,7 @@ async def hi(event):
     android = fake.android_platform_token()
     pc = fake.chrome()
     await event.reply(
-        f"<b><u> Fake Information Generated</b></u>\n<b>Name :-</b><code>{name}</code>\n\n<b>Address:-</b><code>{address}</code>\n\n<b>IP ADDRESS:-</b><code>{ip}</code>\n\n<b>credit card:-</b><code>{cc}</code>\n\n<b>Email Id:-</b><code>{email}</code>\n\n<b>Job:-</b><code>{job}</code>\n\n<b>android user agent:-</b><code>{android}</code>\n\n<b>Pc user agent:-</b><code>{pc}</code>",
+        f"<b><u> Thông tin giả mạo được tạo</b></u>\n<b>Tên :-</b><code>{name}</code>\n\n<b>Địa chỉ:-</b><code>{address}</code>\n\n<b>IP ADDRESS:-</b><code>{ip}</code>\n\n<b>Thẻ tín dụng:-</b><code>{cc}</code>\n\n<b>Email Id:-</b><code>{email}</code>\n\n<b>Công việc:-</b><code>{job}</code>\n\n<b>android user agent:-</b><code>{android}</code>\n\n<b>Pc user agent:-</b><code>{pc}</code>",
         parse_mode="HTML",
     )
 
@@ -44,7 +44,7 @@ async def _(event):
             with open("FRIDAYOT.jpg", "wb") as f:
                 f.write(response.content)
 
-        captin = f"Fake Image powered by @AwesomeSupport."
+        captin = f"Hình ảnh giả mạo được cung cấp bởi @AwesomeSupport."
         fole = "FRIDAYOT.jpg"
         await tbot.send_file(event.chat_id, fole, caption=captin)
         await event.delete()
